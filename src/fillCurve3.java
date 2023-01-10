e91c0ebe51eafc919cb06e91f2cfa6508c0795b6
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2023-01-10 15:02:13 +0100

    add z curve

--- src/fillCurve3.java
+++ src/fillCurve3.java
@@ -1,55 +1,65 @@
 import processing.core.PApplet;
-import processing.core.PFont;
 import processing.core.PVector;
 
-public class fillCurve1 extends PApplet{
-    int level =8    ;
-    int N = (int) pow(2,level);
-    int total = N*N;
+public class fillCurve3 extends PApplet{
+    int level =6;
+    int N = (int)pow(2,level);
+    //    int total =  14;
+    int total =  4 * (int)pow(4,level-1);
+
     PVector[] path = new PVector[total];
 
+
     public void settings(){
-        size(1024,1024);
+        int size = 512;
+        size(size,size);
         smooth();
     }
     public void setup(){
         colorMode(HSB,360,255,255);
         background(0);
         for (int i = 0; i < total; i++) {
-            path[i] = Hilbert(i);
-            float len = width/N ;
+            path[i] = Tri(i);
+            float len = (width /N) ; // fix later - value now: 128
             path[i].mult(len);
-            path[i].add(len/2,len/2);
+           path[i].add(len/2,len/2);
         }
     }
 
-    int counter = 0 ;
+    float counter = 0 ;
     public void draw(){
         background(0);
         stroke(255);
         strokeWeight(2);
 //        beginShape();
-        for (int j = 1; j <  counter; j++) {
+        for (int j = 1; j < counter; j++) {
             float hue = map (j, 0 , path.length,0,360);
             stroke(hue,255,255);
             line(path[j].x,path[j].y,path[j-1].x,path[j-1].y);
-        }
+        } // this is drawing part :V
+        strokeWeight(4);
+//        for (int j = 1; j < counter; j++) {
+//            point(path[j].x,path[j].y);
+////            text((int)path[j-1].x, (float) (path[j-1].x), (float) (path[j-1].y));
+////            text((int)path[j-1].y, (float) (path[j-1].x+10), (float) (path[j-1].y+10));
+////            text((int)j-1, (float) (path[j-1].x), (float) (path[j-1].y));
+//        }
 //        endShape();
-        counter+= 1;
+        counter+= 1; // this is for change the speed of the animation - the higher the more speed
         if (counter >= path.length){
             counter = 0;
         }
         text("Level: "+ level,0,40);
 
-    }
+    } // dont touch it until you done the algo :))))))))
 
 
-    public PVector Hilbert (int i){
+    public PVector Tri(int i){
         PVector[] point = {
                 new PVector(0,0),
+                new PVector(1,0),
                 new PVector(0,1),
-                new PVector(1,1),
-                new PVector(1,0)
+                new PVector(1,1)
         };
         int index = i &3;
         PVector v = point[index];
@@ -59,21 +69,19 @@ public class fillCurve1 extends PApplet{
             i = i >>>2;
             index = i & 3;
             if (index == 0 ){
-                float temp = v.x;
-                v.x = v.y;
-                v.y = temp;
+
             }
             else if (index == 1){
-                v.y += len;
+                v.x += len;
             }
             else if (index == 2){
-                v.x +=len;
-                v.y +=len;
+                v.y += len;
+
             }
             else if (index == 3){
-                float temp = len - 1 - v.x; // len now =
-                v.x = 2* len-1-v.y  ;
-                v.y = temp;
+                v.x +=len;
+                v.y +=len;
+
             }
         }
         return v;
