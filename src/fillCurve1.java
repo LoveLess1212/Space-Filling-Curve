4564aa7c2e7e3e62e3ee4438b8708ad9606c6863
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2023-01-10 13:28:47 +0100

    1st gen

---
+++ src/fillCurve1.java
@@ -0,0 +1,86 @@
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PVector;
+
+public class fillCurve1 extends PApplet{
+    int level =2    ;
+    int N = (int) pow(2,level);
+    int total = N*N;
+    PVector[] path = new PVector[total];
+
+    public void settings(){
+        size(1024,1024);
+        smooth();
+    }
+    public void setup(){
+        colorMode(HSB,360,255,255);
+        background(0);
+        for (int i = 0; i < total; i++) {
+            path[i] = Hilbert(i);
+            float len = width/N ;
+            path[i].mult(len);
+            path[i].add(len/2,len/2);
+        }
+    }
+
+    int counter = 0 ;
+    public void draw(){
+        background(0);
+        stroke(255);
+        strokeWeight(2);
+//        beginShape();
+        for (int j = 1; j <  path.length; j++) {
+            float hue = map (j, 0 , path.length,0,360);
+            stroke(hue,255,255);
+            line(path[j].x,path[j].y,path[j-1].x,path[j-1].y);
+        }
+//        endShape();
+        counter+= 1;
+        if (counter >= path.length){
+            counter = 0;
+        }
+        text("Level: "+ level,0,40);
+
+    }
+
+
+    public PVector Hilbert (int i){
+        PVector[] point = {
+                new PVector(0,0),
+                new PVector(0,1),
+                new PVector(1,1),
+                new PVector(1,0)
+        };
+        int index = i &3;
+        PVector v = point[index];
+
+        for (int j = 1; j < level; j++) {
+            int len = (int)pow(2,j);
+            i = i >>>2;
+            index = i & 3;
+            if (index == 0 ){
+                float temp = v.x;
+                v.x = v.y;
+                v.y = temp;
+            }
+            else if (index == 1){
+                v.y += len;
+            }
+            else if (index == 2){
+                v.x +=len;
+                v.y +=len;
+            }
+            else if (index == 3){
+                float temp = len - 1 - v.x; // len now =
+                v.x = 2* len-1-v.y  ;
+                v.y = temp;
+            }
+        }
+        return v;
+    }
+
+    public static void main(String[] args) {
+        String[] name = new String[] {"fillCurve1"};
+        PApplet.main(name);
+    }
+}
---
+++ src/fillCurve2.java
@@ -0,0 +1,98 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class fillCurve2 extends PApplet{
+    int level =3 ;
+    int N = (int)pow(2,level);
+//    int total =  14;
+    int total =  3 * (int)pow(4,level-1);
+
+    PVector[] path = new PVector[total];
+
+
+    public void settings(){
+        int size = 1024;
+        size(size,size);
+        smooth();
+    }
+    public void setup(){
+        colorMode(HSB,360,255,255);
+        background(0);
+        for (int i = 0; i < total; i++) {
+            path[i] = Tri(i);
+            float len = 2*(512 /N) ; // fix later - value now: 256
+            path[i].mult(len);
+//           path[i].add(0,len);
+        }
+    }
+
+    int counter = 0 ;
+    public void draw(){
+        background(0);
+        stroke(255);
+        strokeWeight(2);
+//        beginShape();
+        for (int j = 1; j < path.length; j++) {
+            float hue = map (j, 0 , path.length,0,360);
+            stroke(hue,255,255);
+            line(path[j].x,path[j].y,path[j-1].x,path[j-1].y);
+        } // this is drawing part :V
+        strokeWeight(4);
+        for (int j = 1; j < path.length; j++) {
+            point(path[j].x,path[j].y);
+            text((int)path[j-1].x, (float) (path[j-1].x), (float) (path[j-1].y));
+            text((int)path[j-1].y, (float) (path[j-1].x+10), (float) (path[j-1].y+10));
+//            text((int)j-1, (float) (path[j-1].x), (float) (path[j-1].y));
+        }
+//        endShape();
+        counter+= 1;
+        if (counter >= path.length){
+            counter = 0;
+        }
+        text("Level: "+ level,0,40);
+
+    } // dont touch it until you done the algo :))))))))
+
+
+    public PVector Tri(int i){
+        PVector[] point = {
+                new PVector(0,0),
+                new PVector(0.5F,0.5F),
+                new PVector(1,0)
+        };
+        int newIndexMod = i %3;
+        i = i /3;
+        PVector v = point[newIndexMod];
+
+        for (int j = 1; j < level; j++) {
+            int len = (int)pow(2,j-1); // now = 1
+
+            int newIndexDiv = i & 3;
+            if (newIndexDiv == 0) {
+                float temp = v.x;
+                v.x = v.y;
+                v.y = temp;
+            }
+            else if (newIndexDiv ==1) {
+                v.y += len;
+            }
+
+            else if (newIndexDiv ==2 ) {
+                v.x +=len;
+                v.y +=len;
+            }
+            else if (newIndexDiv ==3){
+                float temp = 1 - v.x; // len now =
+                v.x = 3* len-1-v.y  ;
+                v.y = temp;
+            }
+            i = i >>>2;
+        }
+        return v;
+    }
+
+    public static void main(String[] args) {
+        String[] name = new String[] {"fillCurve2"};
+        PApplet.main(name);
+    }
+}
